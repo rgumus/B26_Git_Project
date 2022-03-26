e4b67c7eb71cde131c27957d8c479cd04ec19935
rgumus <rachelgumush@gmail.com>
2022-03-26 15:07:39 -0500

    Wrote the code for first class

--- src/day0/First.java
+++ src/day0/First.java
@@ -1,6 +1,10 @@
 package day0;
 
 public class First {
-    //TODO: works on this tomorrow
-    //add some cool code
+//    TODO: works on this tomorrow
+    // add some cool code
+
+    //extra line 1
+    //extra line 2
+    //extra line 3
 }
