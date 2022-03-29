dab53fcf2057f00003c16a08b981904c4b9d7fcb
rgumus <rachelgumush@gmail.com>
2022-03-29 06:30:55 -0500

    Print out is fixed.

--- src/day0/JamesBond.java
+++ src/day0/JamesBond.java
@@ -2,7 +2,7 @@ package day0;
 
 public class JamesBond{
   public static void main(String[] args){
-    System.out.println("James Bond hacked the project);
+    System.out.println("James Bond hacked the project");
   }
 }
                        
